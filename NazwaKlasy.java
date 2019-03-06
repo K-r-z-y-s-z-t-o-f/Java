b54141707f7990747d89399b0abeecccd17952ad
K-r-z-y-s-z-t-o-f <31901548+K-r-z-y-s-z-t-o-f@users.noreply.github.com>
2019-03-06 01:26:38 +0100

    Add files via upload

---
+++ NazwaKlasy.java
@@ -0,0 +1,7 @@
+class NazwaKlasy
+{
+ public static void main ( String[] args )
+ {
+  System.out.println("Witaj Javo!");
+ }
+}
---
+++ PierwszyProgram.java
@@ -0,0 +1,11 @@
+class PierwszyProgram
+{
+ public static void main ( String[] args )
+ {
+  System.out.println(" _ _ ");
+  System.out.println("|_|_|");
+  System.out.println("|_|_|");
+  System.out.println (""); 
+  System.out.println("K≥opoty z polskimi znakami •∆ £”åØè");
+ }
+}
